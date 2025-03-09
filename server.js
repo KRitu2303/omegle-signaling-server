a771d739bf1c1f7f72f790d0b77cfff93176ac96
KRitu2303 <109243616+KRitu2303@users.noreply.github.com>
2025-03-09 15:34:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,34 +4,44 @@ const server = new WebSocket.Server({ port: 3000 });
 let waitingUser = null;
 
 server.on("connection", (socket) => {
-  socket.on("message", (message) => {
-    let data = JSON.parse(message);
+    console.log("New user connected");
 
-    if (data.type === "find_match") {
-      if (waitingUser) {
-        socket.partner = waitingUser;
-        waitingUser.partner = socket;
+    socket.on("message", (message) => {
+        let data = JSON.parse(message);
 
-        let offer = { type: "offer", offer: waitingUser.offer };
-        socket.send(JSON.stringify(offer));
-        waitingUser.send(JSON.stringify({ type: "match_found" }));
+        if (data.type === "find_match") {
+            if (waitingUser) {
+                socket.partner = waitingUser;
+                waitingUser.partner = socket;
 
-        waitingUser = null;
-      } else {
-        waitingUser = socket;
-      }
-    }
+                let offer = { type: "offer", offer: waitingUser.offer };
+                socket.send(JSON.stringify(offer));
+                waitingUser.send(JSON.stringify({ type: "match_found" }));
 
-    if (data.type === "offer") {
-      socket.offer = data.offer;
-    }
-  });
+                waitingUser = null;
+            } else {
+                waitingUser = socket;
+            }
+        }
 
-  socket.on("close", () => {
-    if (waitingUser === socket) {
-      waitingUser = null;
-    }
-  });
+        if (data.type === "offer") {
+            socket.offer = data.offer;
+        }
+
+        if (data.type === "answer" && socket.partner) {
+            socket.partner.send(JSON.stringify({ type: "answer", answer: data.answer }));
+        }
+
+        if (data.type === "ice-candidate" && socket.partner) {
+            socket.partner.send(JSON.stringify({ type: "ice-candidate", candidate: data.candidate }));
+        }
+    });
+
+    socket.on("close", () => {
+        if (waitingUser === socket) {
+            waitingUser = null;
+        }
+    });
 });
 
 console.log("Signaling Server is running on port 3000");
